89c5385230c688e10292259c9c995ae7f1c8b4c2
Thomas Kolanko <Thomas.Kolanko@ca.ibm.com>
2020-06-29 11:44:33 -0400

    add license to samples

--- db2_graph/README.md
+++ db2_graph/README.md
@@ -39,3 +39,10 @@ To load the sample graph data:
         jupyter notebook
 
 7. To setup IBM Db2 Graph please see the technote at https://www.ibm.com/support/pages/node/6205946
+
+
+## License
+
+This code pattern is licensed under the Apache License, Version 2. Separate third-party code objects invoked within this code pattern are licensed by their respective providers pursuant to their own separate licenses. Contributions are subject to the [Developer Certificate of Origin, Version 1.1](https://developercertificate.org/) and the [Apache License, Version 2](https://www.apache.org/licenses/LICENSE-2.0.txt).
+
+[Apache License FAQ](https://www.apache.org/foundation/license-faq.html#WhatDoesItMEAN)
--- db2_graph/data/03_foreignKeys.sql
+++ db2_graph/data/03_foreignKeys.sql
@@ -1,3 +1,29 @@
+-- -- #----------------------------------------------------------------------------------------------#
+-- #  NAME:     03_foreignKeys.sql                                                                #
+-- #                                                                                              #
+-- #----------------------------------------------------------------------------------------------#
+-- #                     DISCLAIMER OF WARRANTIES AND LIMITATION OF LIABILITY                     #
+-- #                                                                                              #
+-- #  (C) COPYRIGHT International Business Machines Corp. 2018, 2019 All Rights Reserved          #
+-- #  Licensed Materials - Property of IBM                                                        #
+-- #                                                                                              #
+-- #  US Government Users Restricted Rights - Use, duplication or disclosure restricted by GSA    #
+-- #  ADP Schedule Contract with IBM Corp.                                                        #
+-- #                                                                                              #
+-- #  The following source code ("Sample") is owned by International Business Machines            #
+-- #  Corporation ("IBM") or one of its subsidiaries and is copyrighted and licensed, not sold.   #
+-- #  You may use, copy, modify, and distribute the Sample in any form without payment to IBM,    #
+-- #  for the purpose of assisting you in the creation of Python applications using the ibm_db    #
+-- #  library.                                                                                    #
+-- #                                                                                              #
+-- #  The Sample code is provided to you on an "AS IS" basis, without warranty of any kind. IBM   #
+-- #  HEREBY EXPRESSLY DISCLAIMS ALL WARRANTIES, EITHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT    #
+-- #  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. #
+-- #  Some jurisdictions do not allow for the exclusion or limitation of implied warranties, so   #
+-- #  the above limitations or exclusions may not apply to you. IBM shall not be liable for any   #
+-- #  damages you suffer as a result of using, copying, modifying or distributing the Sample,     #
+-- #  even if IBM has been advised of the possibility of such damages.                            #
+-- #----------------------------------------------------------------------------------------------#
 
 -- DDL Statements for Foreign Keys on Table "DEMO    "."INCHARGE"
 ALTER TABLE "DEMO    "."INCHARGE" 
